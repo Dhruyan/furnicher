39c6161bde5f5e1c3d4540509e48d88f7da5d1ff
dhruyan baldaniya <baldaniyadhruyan3@gmail.com>
2023-10-05 11:54:19 +0530

    Initial commit

---
+++ lib/view/login/boarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Boarding extends StatelessWidget {
+  const Boarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: do,
+      ),
+    );
+  }
+}
