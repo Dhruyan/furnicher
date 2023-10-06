0c62a9915ae0e9bf3b5e5f063bdd8246c40229c9
dhruyan baldaniya <baldaniyadhruyan3@gmail.com>
2023-10-06 12:11:59 +0530

    changes

---
+++ lib/res/contrest/app_assets.dart
@@ -0,0 +1,26 @@
+class Appassets {
+  static const String bordings = "assets/images/bordings.png";
+  static const String back = "assets/images/back.png";
+  static const String star = "assets/images/bordings.png";
+  static const String chair = "assets/images/bordings.png";
+  static const String table = "assets/images/bordings.png";
+  static const String aramchair = "assets/images/bordings.png";
+  static const String bed = "assets/images/bordings.png";
+  static const String lamp = "assets/images/bordings.png";
+  static const String simpelamp = "assets/images/bordings.png";
+  static const String minimalstand = "assets/images/bordings.png";
+  static const String coffeechair = "assets/images/bordings.png";
+  static const String simpledesk = "assets/images/bordings.png";
+  static const String product = "assets/images/bordings.png";
+  static const String favourite1 = "assets/images/bordings.png";
+  static const String favourite2 = "assets/images/bordings.png";
+  static const String favourite3 = "assets/images/bordings.png";
+  static const String favourite4 = "assets/images/bordings.png";
+  static const String cart2 = "assets/images/bordings.png";
+  static const String cart3 = "assets/images/bordings.png";
+  static const String cart4 = "assets/images/bordings.png";
+  static const String = "assets/images/bordings.png";
+  static const String = "assets/images/bordings.png";
+  static const String = "assets/images/bordings.png";
+  static const String = "assets/images/bordings.png";
+}
